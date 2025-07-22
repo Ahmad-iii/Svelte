adaebc242aef46880866793e6d3fae26343ba69d
Ahmad-iii <ahmad23nov@gmail.com>
2025-07-22 22:30:20 +0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+
+
+<div class="background">
+  <h1 class="text-3xl font-bold text-red-500">Welcome too SvelteKit</h1>
+  <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
+
+<style>
+  .background {
+    min-height: 100vh;
+    min-width: 100vw;
+    width: 100vw;
+    height: 100vh;
+    background-image: url('/header_background.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
